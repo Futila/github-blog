dbaaf5f18a2206a0fed7719d881eb4a70db1f523
Fernando Futila <fernando.futila@gmail.com>
2024-05-30 13:14:24 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { Globalstyle } from "./styles/global";
+import { defaultTheme } from "./styles/theme/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+    <h1>Hello world</h1>
+      <Globalstyle/>
+    </ThemeProvider>
+  )
+}
+
+
